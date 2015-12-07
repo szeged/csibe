c6050a68fc4ee590434ba41d9c0e12ec323fc83f
Gabor Loki <loki@inf.u-szeged.hu>
2015-12-07 14:03:08 +0100

    Add basic framework and two sample projects

---
+++ gen/flex-2.6.0/config.h
@@ -0,0 +1,107 @@
+#define ENABLE_NLS 1
+
+#define HAVE_ALLOCA 1
+
+#define HAVE_ALLOCA_H 1
+
+#define HAVE_DCGETTEXT 1
+
+#define HAVE_DLFCN_H 1
+
+#define HAVE_DUP2 1
+
+#define HAVE_FORK 1
+
+#define HAVE_GETTEXT 1
+
+#define HAVE_INTTYPES_H 1
+
+#define HAVE_ISASCII 1
+
+#define HAVE_LIBINTL_H 1
+
+#define HAVE_LIBM 1
+
+#define HAVE_LIBPTHREAD 1
+
+#define HAVE_LIMITS_H 1
+
+#define HAVE_LOCALE_H 0
+
+#define HAVE_MALLOC 1
+
+#define HAVE_MALLOC_H 1
+
+#define HAVE_MEMORY_H 1
+
+#define HAVE_MEMSET 1
+
+#define HAVE_NETINET_IN_H 1
+
+#define HAVE_POW 1
+
+#define HAVE_PTHREAD_H 1
+
+#define HAVE_REALLOC 1
+
+#define HAVE_REGCOMP 1
+
+#define HAVE_REGEX_H 1
+
+#define HAVE_SETLOCALE 1
+
+#define HAVE_STDBOOL_H 1
+
+#define HAVE_STDDEF_H 1
+
+#define HAVE_STDINT_H 1
+
+#define HAVE_STDLIB_H 1
+
+#define HAVE_STRCHR 1
+
+#define HAVE_STRINGS_H 1
+
+#define HAVE_STRING_H 1
+
+#define HAVE_STRTOL 1
+
+#define HAVE_SYS_STAT_H 1
+
+#define HAVE_SYS_TYPES_H 1
+
+#define HAVE_SYS_WAIT_H 1
+
+#define HAVE_UNISTD_H 1
+
+#define HAVE_VFORK 1
+
+#define HAVE_WORKING_FORK 1
+
+#define HAVE_WORKING_VFORK 1
+
+#define HAVE__BOOL 1
+
+#define LT_OBJDIR ".libs/"
+
+#define M4 "/usr/bin/m4"
+
+#define PACKAGE "flex"
+
+#define PACKAGE_BUGREPORT "flex-help@lists.sourceforge.net"
+
+#define PACKAGE_NAME "the fast lexical analyser generator"
+
+#define PACKAGE_STRING "the fast lexical analyser generator 2.6.0"
+
+#define PACKAGE_TARNAME "flex"
+
+#define PACKAGE_URL ""
+
+#define PACKAGE_VERSION "2.6.0"
+
+#define STDC_HEADERS 1
+
+#define VERSION "2.6.0"
+
+#define YYTEXT_POINTER 1

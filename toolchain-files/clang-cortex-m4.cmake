9ab07b5e96100105817626040065839478d25859
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2015-12-16 14:42:26 +0100

    Add toolchain files for Cortex-M0/M4 cross-compilation
    These toolchain files contain the necessary steps that enable cross-compile
    for Cortex-M0/M4 targets using Clang or GCC as compilers.
    
    Note: arm-none-eabi-gcc toolchain is needed for this to work.
    The 'bin' subdirectory of the toolchain has to be in your PATH.
    
    Usage: Add -DCMAKE_TOOLCHAIN_FILE=path/to/csibe/toolchain/toolchain.file
    to cmake options.

---
+++ toolchain-files/clang-cortex-m4.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Generic)
+
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+
+set(triple arm-none-eabi)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET ${triple})
+
+set(CORTEX_M4_FLAGS "-Os -mcpu=cortex-m4 -mthumb")
+set(CMAKE_C_FLAGS "${CORTEX_M4_FLAGS}" CACHE STRING "" FORCE)

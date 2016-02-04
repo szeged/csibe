26861b183085e8fe2c7c21f4e3631ddd7d30e5e8
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-02-04 16:02:19 +0100

    Add logic and error-handling for CMake and make invocations

--- csibe.py
+++ csibe.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import subprocess
-import unittest
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-j", "--jobs", type=int, default=1, help="number of jobs for make")
+args = parser.parse_args()
+
+make_jobs = args.jobs
 
 csibe_path = os.path.dirname(os.path.realpath(__file__))
 build_directory = "build"
@@ -12,5 +19,15 @@ if not os.path.isdir(build_directory):
 
 os.chdir(build_directory)
 
-subprocess.call(["cmake", csibe_path])
+cmake_return_value = subprocess.call(["cmake", csibe_path])
+if cmake_return_value:
+    sys.exit(cmake_return_value)
+
+make_return_value = subprocess.call(["make", "-j{}".format(make_jobs)])
+if make_return_value:
+    sys.exit(make_return_value)
+
+make_size_return_value =  subprocess.call(["make", "size"])
+if make_size_return_value:
+    sys.exit(make_size_return_value)
 

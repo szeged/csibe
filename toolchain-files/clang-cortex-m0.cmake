f2f5ec00bbdb24bf849fdc3539b83d8f8c04258a
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-03-03 13:46:27 +0100

    Add C++ support to the toolchain files

--- toolchain-files/clang-cortex-m0.cmake
+++ toolchain-files/clang-cortex-m0.cmake
@@ -7,6 +7,8 @@ set(CMAKE_CXX_COMPILER_WORKS 1)
 set(triple arm-none-eabi)
 
 set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
 
 set(CORTEX_M0_FLAGS "--target=${triple} -Os -mcpu=cortex-m0 -mthumb")
 set(CMAKE_C_FLAGS "${CORTEX_M0_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${CORTEX_M0_FLAGS}" CACHE STRING "" FORCE)

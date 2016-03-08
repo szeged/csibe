f3f6a1403de394c9157071cccac2bf12d4424b66
Gabor Loki <loki@inf.u-szeged.hu>
2016-03-08 15:03:00 +0100

    Support various compiler flags in project Servo

---
+++ gen/servo/compiler.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
+import os
+
+if __name__ == '__main__':
+    def which_next(program, old):
+        def is_exe(fpath):
+            return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.abspath(os.path.join(path, program))
+            if is_exe(exe_file) and exe_file != old:
+                return exe_file
+        return None
+
+    optflags = ['-O', '-O0', '-O1', '-O2', '-O3', '-O4', '-Os', '-Oz']
+
+    basename = os.path.basename(sys.argv[0])
+    abspath = os.path.abspath(sys.argv[0])
+    next = which_next(basename, abspath);
+    if (not next):
+        logging.error("Missing '" + basename + "' from PATH!")
+        sys.exit(1)
+
+    clearopts=False
+    if basename.endswith("g++") or basename.endswith("clang++"):
+        flags = os.getenv('CSiBE_CXXFLAGS','').split()
+        ext = ['.cxx','.cpp']
+    elif basename.endswith("rustc"):
+        flags = os.getenv('CSiBE_RUSTCFLAGS','').split()
+        ext = ['.rs']
+    else:
+        flags = os.getenv('CSiBE_CFLAGS','').split()
+        ext = ['.c']
+
+    if flags and len(flags) > 0 and flags[0] == "!":
+        clearopts = True
+        flags = flags[1:]
+
+    done = False
+    srcfile = False
+
+    cmd = [next]
+
+    for arg in sys.argv[1:]:
+        if "--" == arg:
+            cmd.extend(flags)
+            done = True
+        elif [elem for elem in ext if elem in arg]:
+            srcfile = True
+
+        if (not clearopts) or (arg not in optflags):
+            cmd.append(arg)
+
+    if srcfile:
+        if not done:
+            cmd.extend(flags)
+    else:
+        cmd = [next]
+        cmd.extend(sys.argv[1:])
+
+    sys.exit(subprocess.call(cmd))
--- gen/servo/rustc-wrapper.py
+++ gen/servo/rustc-wrapper.py
@@ -5,25 +5,25 @@ import subprocess
 import os
 
 if __name__ == '__main__':
-	done = False
-	rsfile = False
-	addopts = os.getenv('CSiBE_RUSTCFLAGS','').split()
+    done = False
+    rsfile = False
+    addopts = os.getenv('CSiBE_RUSTCFLAGS','').split()
 
-	cmd = ['rustc-orig']
+    cmd = ['rustc-orig']
 
-	for arg in sys.argv[1:]:
-		if "--" == arg:
-			cmd.extend(addopts)
-			done = True
-		elif ".rs" in arg:
-			rsfile = True
-		cmd.append(arg)
+    for arg in sys.argv[1:]:
+        if "--" == arg:
+            cmd.extend(addopts)
+            done = True
+        elif ".rs" in arg:
+            rsfile = True
+        cmd.append(arg)
 
-	if rsfile:
-		if not done:
-			cmd.extend(addopts)
-	else:
-		cmd = ['rustc-orig']
-		cmd.extend(sys.argv[1:])
+    if rsfile:
+        if not done:
+            cmd.extend(addopts)
+    else:
+        cmd = ['rustc-orig']
+        cmd.extend(sys.argv[1:])
 
-	sys.exit(subprocess.call(cmd))
+    sys.exit(subprocess.call(cmd))

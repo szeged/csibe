b3061e0cf6d04e4835977682cad483f89068269e
Gabor Loki <loki@inf.u-szeged.hu>
2016-03-11 14:45:19 +0100

    Move CSiBE's variables to a separate file

---
+++ cmake/csibe-env.cmake
@@ -0,0 +1,11 @@
+set (CSiBE_VERSION_MAJOR 3)
+set (CSiBE_VERSION_MINOR 0)
+set (CSiBE_PATCH_VERSION 0)
+
+set (CSiBE_BIN_DIR ${CSiBE_DIR}/bin)
+set (CSiBE_GEN_DIR ${CSiBE_DIR}/gen)
+set (CSiBE_SRC_DIR ${CSiBE_DIR}/src)
+
+set (DUMP_OBJ_SIZE ${CSiBE_BIN_DIR}/dump_obj_size)
+set (SUM_SIZES     ${CSiBE_BIN_DIR}/sum_sizes)
+set (SUM_RESULT    ${CSiBE_BIN_DIR}/sum_results)

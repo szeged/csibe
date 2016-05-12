a43f12aa5406ce6d90fef538c7cb84c26d017c6d
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-05-12 20:17:46 +0200

    Add 'OpenTCP-1.0.4' subproject to old CSiBE testbed

--- cmake/subprojects.cmake
+++ cmake/subprojects.cmake
@@ -33,7 +33,8 @@ function(adopt_subprojects)
                 CSiBE-v2.1.1/libmspack
                 CSiBE-v2.1.1/libpng-1.2.5
                 CSiBE-v2.1.1/mpgcut-1.1
-                CSiBE-v2.1.1/mpeg2dec-0.3.1)
+                CSiBE-v2.1.1/mpeg2dec-0.3.1
+                CSiBE-v2.1.1/OpenTCP-1.0.4)
         set(SUBPROJECTS ${SUBPROJECTS} PARENT_SCOPE)
         set(OLD_CSIBE_BASE_DIR ${CSiBE_SRC_DIR}/CSiBE-v2.1.1 PARENT_SCOPE)
     endif()

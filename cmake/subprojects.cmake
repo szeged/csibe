b0d453f256eb379f2a66c97b7fb8797c39ce58bd
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-05-13 13:48:29 +0200

    Add "teem-1.6.0-src" subproject to old CSiBE testbed

--- cmake/subprojects.cmake
+++ cmake/subprojects.cmake
@@ -34,7 +34,8 @@ function(adopt_subprojects)
                 CSiBE-v2.1.1/libpng-1.2.5
                 CSiBE-v2.1.1/mpgcut-1.1
                 CSiBE-v2.1.1/mpeg2dec-0.3.1
-                CSiBE-v2.1.1/OpenTCP-1.0.4)
+                CSiBE-v2.1.1/OpenTCP-1.0.4
+                CSiBE-v2.1.1/teem-1.6.0-src)
         set(SUBPROJECTS ${SUBPROJECTS} PARENT_SCOPE)
         set(OLD_CSIBE_BASE_DIR ${CSiBE_SRC_DIR}/CSiBE-v2.1.1 PARENT_SCOPE)
     endif()

7676c0bf60b047df0676ad6df53f10ee28876bd3
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-05-13 14:07:44 +0200

    Add 'unrarlib-0.4.0' subproject to old CSiBE testbed

--- cmake/subprojects.cmake
+++ cmake/subprojects.cmake
@@ -35,7 +35,8 @@ function(adopt_subprojects)
                 CSiBE-v2.1.1/mpgcut-1.1
                 CSiBE-v2.1.1/mpeg2dec-0.3.1
                 CSiBE-v2.1.1/OpenTCP-1.0.4
-                CSiBE-v2.1.1/teem-1.6.0-src)
+                CSiBE-v2.1.1/teem-1.6.0-src
+                CSiBE-v2.1.1/unrarlib-0.4.0)
         set(SUBPROJECTS ${SUBPROJECTS} PARENT_SCOPE)
         set(OLD_CSIBE_BASE_DIR ${CSiBE_SRC_DIR}/CSiBE-v2.1.1 PARENT_SCOPE)
     endif()

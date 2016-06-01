503a703ab0c0c097bd1f281988961c13c8ec227e
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-06-01 20:58:00 +0200

    Add size_result.py script to lwip subproject

---
+++ gen/CSiBE-v2.1.1/lwip-0.5.3.preproc/size_result.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+if __name__ == "__main__":
+
+    csibe_binary_dir = os.environ["CSiBE_BIN_DIR"]
+    project_binary_dir = os.environ["PROJECT_BINARY_DIR"]
+
+    dump_obj_size_script = os.path.join(csibe_binary_dir, "dump_obj_size")
+
+    for file in os.listdir(project_binary_dir):
+        subprocess.call([dump_obj_size_script, file, project_binary_dir])

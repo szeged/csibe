326ac23ef8f92ff9f82c12361888b45341be53b1
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2016-06-21 11:54:36 +0000

    Revert "Disable auto-running"
    This reverts commit a71c563a752d9986fbb1573909150b3afcee068e.

--- Dockerfile.setup.sh
+++ Dockerfile.setup.sh
@@ -6,12 +6,12 @@ echo 'export SHELL="/bin/bash"' >> .bashrc
 
 export SHELL="/bin/bash"
 
-#python --version
+python --version
 
-#git clone https://github.com/szeged/csibe
-#cd csibe
+git clone https://github.com/szeged/csibe
+cd csibe
 
-#./csibe.py native -Os -w
-#./csibe.py gcc-cortex-m0 gcc-cortex-m4 clang-cortex-m0 clang-cortex-m4 -Os -w
-#./csibe.py native CSiBE-v2.1.1 -Os -w
-#./csibe.py native servo
+./csibe.py native -Os -w
+./csibe.py gcc-cortex-m0 gcc-cortex-m4 clang-cortex-m0 clang-cortex-m4 -Os -w
+./csibe.py native CSiBE-v2.1.1 -Os -w
+./csibe.py native servo
